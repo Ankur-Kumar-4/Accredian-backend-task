61cdc5ad4546fe1c893c1ca02a927b36d2c9b677
Ankurdyno <98178905+Ankurdyno@users.noreply.github.com>
2024-07-20 13:56:21 +0530

    chore: add cors

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,14 @@ import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.routes';
 import referralRouter from './routes/referral.routes';
+import cors from 'cors';
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.json());
 app.use('/user', userRouter);
 app.use('/referral', referralRouter);
